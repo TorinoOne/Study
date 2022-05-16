ac35f87977e00b5faf55a6b9f58e73e883414883
unknown <torinoone1996@gmail.com>
2022-05-16 17:18:00 -0300

    16-5-22 17:18

--- JS/practica3/practica3A.js
+++ JS/practica3/practica3A.js
@@ -1,7 +1,7 @@
 //Mostrar 3 celulares y sus propiedades
 //Cada uno debe tener color, peso, resolucion de pantalla de camara,
 //    resolucion de pantalla y memoria RAM
-//Deben poder prender, reiniciar, tomar fotos y grabar
+//Deben poder prender, reiniciar, tomar fotos y grabar ///COMPLETADO
 
 //Implementar todas estas cualidades en celulares de alta
 //    gama. Graban en camara lenta, tienen reconocimiento facial
@@ -38,16 +38,66 @@ class Celular {
     }
     tomarFoto () {
         alert(`foto tomada en resolucion de: ${this.resolucionCamara}`)
-
+ 
     }
     grabarVideo () {
         alert(`grabando video en ${this.resolucionCamara}`)
     }
+    info(){
+        return `
+        Color: ${this.color} </br>
+        Peso: ${this.peso}</br>
+        Resolucion de camara: ${this.resolucionCamara} </br>
+        Resolucion de pantalla: ${this.resolucionPantalla} </br>
+        Memoria RAM: ${this.memoriaRam} </br>
+        `
+    }
 }
 
-celularTukiTuki = new Celular ("azul", "150g", "full hd", "1080", "8 ram")
+class CelularAltaGama extends Celular { //Herencia
+    constructor(color, peso, rdc, rdp, ram, ce) {
+        super(color, peso, rdc, rdp, ram);
+        this.resolucionCamaraExtra = ce;
+    } camaraLenta () { 
+        alert(`Grabando en camara lenta`)
+    } reconocimientoFacial () {
+        alert(`Iniciar reconocimiento facial`)
+    } infoAltaGama () {
+        return this.info() + `Resolucion camara extra: ${this.resolucionCamaraExtra}`
+     } 
 
-botonEncendido();
-opcionReiniciar();
-tomarFoto();
-grabarVideo();
+} //Todos los metodos podrian tener el codigo correspondiente para realizar lo que indica su nombre
+        //pero recordar que esto es una practica. Lo complejo viene despues pero sera aplicado con la misma logica
+ 
+celularTukiTuki = new Celular ("azul", "150g", "full hd", "1080", "8 ram");
+celularPuca = new Celular ("rojo", "140g", "full hd", "720", "6 ram");
+celularPunk = new Celular ("negro", "130g", "full hd", "1200", "6 ram" );
+
+celularRush = new CelularAltaGama ("blanco", "100g", "full hd", "4k", "10 ram");
+celularTuco = new CelularAltaGama ("negro", "120g", "full hd", "4k", "16 ram");
+ 
+//Llamado de metodos----------
+// botonEncendido();
+// opcionReiniciar();
+// tomarFoto();
+// grabarVideo();
+// botonEncendido();
+
+//reconocimientoFacial();
+//infoAltaGama(); 
+//----------------------------
+
+
+
+
+document.write(`
+    ${celularTukiTuki.info()}<br>
+    ${celularPuca.info()}<br>
+    ${celularPuca.info()}<br>
+
+`)
+
+document.write(`
+    ${celularRush.infoAltaGama()}<br>
+    ${celularTuco.infoAltaGama()}<br>
+`)
